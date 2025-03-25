8e585ef4c56fa28b7c097b0ac5edca1da4addf0d
KulikovaSasha <kulikova.sasha@gmail.com>
2025-03-25 17:07:32 +0300

    TBot

--- main.py
+++ main.py
@@ -13,7 +13,9 @@ bot = telebot.TeleBot(TG_TOKEN)
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.send_message(message.chat.id, f'Привет, {message.from_user.first_name}! Я умею добавляте текст на фото. Если хочешь попробовать, отправь картинку')
+    bot.send_message(message.chat.id,
+                     f'Привет, {message.from_user.first_name}! Я умею добавляте текст на фото. Если хочешь попробовать, отправь картинку')
+
 
 # обработчик сообщений с фотографиями
 @bot.message_handler(content_types=['photo'])
@@ -28,10 +30,10 @@ def handle_photo(message):
     btn1 = types.InlineKeyboardButton('Вверху', callback_data='top')
     btn2 = types.InlineKeyboardButton('Внизу', callback_data='bottom')
     markup.row(btn1, btn2)
-    bot.reply_to(message, f"{message.from_user.first_name}, отличное фото! Давай решим, где будет находиться надпись", reply_markup=markup)
+    bot.reply_to(message, f"{message.from_user.first_name}, отличное фото! Давай решим, где будет находиться надпись",
+                 reply_markup=markup)
 
     @bot.callback_query_handler(func=lambda callback: True)
-
     def callback_message(callback):
         if callback.data == 'top':
             bot.send_message(message.chat.id, f"Хорошо! Теперь отправь мне текст")
@@ -45,19 +47,20 @@ def handle_photo(message):
 
 
 # обработчик текста
-def set_photo_text(message,position):
+def set_photo_text(message, position):
     try:
         image = Image.open('photo.jpg')
         draw = ImageDraw.Draw(image)
         # загрузка шрифта, поддерживающего кириллицу
 
         h = 60
-        font = ImageFont.truetype('CactusClassicalSerif-Regular.ttf', h)  # убеждаемся, что файл находится в рабочем каталоге
+        font = ImageFont.truetype('CactusClassicalSerif-Regular.ttf',
+                                  h)  # убеждаемся, что файл находится в рабочем каталоге
 
-        #накладываем текст на фото
+        # накладываем текст на фото
         text = message.text
         bbox = draw.textbbox((0, 0), text.upper(), font=font)
-        #text_width = bbox[2] - bbox[0]
+        # text_width = bbox[2] - bbox[0]
 
         ans = []
         text_2 = text.split()
@@ -96,8 +99,8 @@ def set_photo_text(message,position):
         for i in ans:
             bbox = draw.textbbox((0, 0), i, font=font)
             text_width = bbox[2] - bbox[0]
-            #text_height = bbox[3] - bbox[1]
-            b = (h+20) * n
+            # text_height = bbox[3] - bbox[1]
+            b = (h + 20) * n
             x = (image.width - text_width) / 2
             if position == 'bottom':
                 y = image.height - b
@@ -106,16 +109,24 @@ def set_photo_text(message,position):
                 y = 20 + b
                 n += 1
 
-            draw.rectangle((x - 8, y, x + text_width + 10, y + h*1.2), fill=(255, 255, 255, 255))
+            draw.rectangle((x - 8, y, x + text_width + 10, y + h * 1.2), fill=(255, 255, 255, 255))
             draw.text((x, y), i, font=font, fill=(0, 0, 0))
             image.save('result.jpg')
 
         bot.send_photo(message.chat.id, open('result.jpg', 'rb'))
+
+        # оцениваем результат
+        markup = types.InlineKeyboardMarkup()
+        btn3 = types.InlineKeyboardButton('Да', callback_data='yes')
+        btn4 = types.InlineKeyboardButton('Нет', callback_data='no')
+        markup.row(btn3, btn4)
+        bot.send_message(message.chat.id, f"{message.from_user.first_name}, готово! Оставляем такой результат?",
+                         reply_markup=markup)
+
 
     except Exception as e:
         bot.send_message(message.chat.id, 'Произошла ошибка, попробуй снова')
         print(e)
 
 
-
-bot.polling(non_stop=True) #запуск бота
+bot.polling(non_stop=True)  # запуск бота
