a0f8d81656baea77fb1e5032c3c3d59b5b849649
KulikovaSasha <kulikova.sasha@gmail.com>
2025-03-25 11:39:20 +0300

    TBot

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 # https://replit.com/@vmgoryachkin/IntelligentShoddyCgibin#main.py
 # https://desktop.telegram.org/
-
+import os
+from background import keep_alive #импорт функции для поддержки работоспособности
+import pip
+pip.main(['install', 'pytelegrambotapi'])
 import telebot
+import time
 from telebot import types
 from PIL import Image, ImageDraw, ImageFont
+from telebot.types import InlineKeyboardButton
+
 from settings import TG_TOKEN
 
 # инициализация бота
@@ -12,8 +18,7 @@ bot = telebot.TeleBot(TG_TOKEN)
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.send_message(message.chat.id, f'Привет, {message.from_user.first_name}! Я - генератор мемов. Для начала отправь картинку - основу мема.')
-
+    bot.send_message(message.chat.id, f'Привет, {message.from_user.first_name}! Я умею добавляте текст на фото. Если хочешь попробовать, отправь картинку')
 
 # обработчик сообщений с фотографиями
 @bot.message_handler(content_types=['photo'])
@@ -23,21 +28,41 @@ def handle_photo(message):
     with open('photo.jpg', 'wb') as new_file:
         new_file.write(downloaded_file)
 
-    bot.send_message(message.chat.id, f"{message.from_user.first_name}, отличное фото! Теперь давай добавим текст")
-    bot.register_next_step_handler(message, set_photo_text)
+    # определяем, где будет текст
+    markup = types.InlineKeyboardMarkup()
+    btn1 = types.InlineKeyboardButton('Вверху', callback_data='top')
+    btn2 = types.InlineKeyboardButton('Внизу', callback_data='bottom')
+    markup.row(btn1, btn2)
+    bot.reply_to(message, f"{message.from_user.first_name}, отличное фото! Давай решим, где будет находиться надпись", reply_markup=markup)
+
+    @bot.callback_query_handler(func=lambda callback: True)
+
+    def callback_message(callback):
+        if callback.data == 'top':
+            bot.send_message(message.chat.id, f"Хорошо! Теперь отправь мне текст")
+            position = 'top'
+            bot.register_next_step_handler(message, set_photo_text, position)
+
+        elif callback.data == 'bottom':
+            bot.send_message(message.chat.id, f"Хорошо! Теперь отправь мне текст")
+            position = 'bottom'
+            bot.register_next_step_handler(message, set_photo_text, position)
 
 
-# обработчик текста для мема
-def set_photo_text(message):
+# обработчик текста
+def set_photo_text(message,position):
     try:
         image = Image.open('photo.jpg')
         draw = ImageDraw.Draw(image)
         # загрузка шрифта, поддерживающего кириллицу
+
         h = 60
-        font = ImageFont.truetype('ARIAL.TTF', h)  # убеждаемся, что файл находится в рабочем каталоге
+        font = ImageFont.truetype('CactusClassicalSerif-Regular.ttf', h)  # убеждаемся, что файл находится в рабочем каталоге
+
+        #накладываем текст на фото
         text = message.text
         bbox = draw.textbbox((0, 0), text.upper(), font=font)
-        text_width = bbox[2] - bbox[0]
+        #text_width = bbox[2] - bbox[0]
 
         ans = []
         text_2 = text.split()
@@ -64,22 +89,29 @@ def set_photo_text(message):
                     text = text + " " + text_2[i]
                     i += 1
 
-
             else:
                 text = text.upper()
                 ans.append(text)
                 go = False
-        n = len(ans)
+        if position == 'bottom':
+            n = len(ans)
+        elif position == 'top':
+            n = 0
 
         for i in ans:
             bbox = draw.textbbox((0, 0), i, font=font)
             text_width = bbox[2] - bbox[0]
-            text_height = bbox[3] - bbox[1]
+            #text_height = bbox[3] - bbox[1]
             b = (h+20) * n
             x = (image.width - text_width) / 2
-            y = image.height - b
-            n -= 1
-            draw.rectangle((x - 10, y, x + text_width + 10, y + h*1.1 ), fill=(255, 255, 255, 255))
+            if position == 'bottom':
+                y = image.height - b
+                n -= 1
+            elif position == 'top':
+                y = 20 + b
+                n += 1
+
+            draw.rectangle((x - 8, y, x + text_width + 10, y + h*1.2), fill=(255, 255, 255, 255))
             draw.text((x, y), i, font=font, fill=(0, 0, 0))
             image.save('result.jpg')
 
@@ -90,4 +122,6 @@ def set_photo_text(message):
         print(e)
 
 
-bot.polling(none_stop=True)
+
+keep_alive()#запускаем flask-сервер в отдельном потоке. Подробнее ниже...
+bot.polling(non_stop=True, interval=0) #запуск бота
