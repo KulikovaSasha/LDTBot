190e4861ef2d6a09b017be07b58b1933841c8463
KulikovaSasha <kulikova.sasha@gmail.com>
2025-03-31 18:33:45 +0300

    TBot

--- main.py
+++ main.py
@@ -14,9 +14,10 @@ bot = telebot.TeleBot(TG_TOKEN)
 @bot.message_handler(commands=['start'])
 def start(message):
     bot.send_message(message.chat.id,
-                     f'Привет, {message.from_user.first_name}! Я умею добавляте текст на фото. Если хочешь попробовать, отправь картинку')
+                     f'Привет, {message.from_user.first_name}! Я умею добавлять текст на фото. Если хочешь попробовать, отправь картинку')
 
-
+text = 0
+text_3 = 0
 # обработчик сообщений с фотографиями
 @bot.message_handler(content_types=['photo'])
 def handle_photo(message):
@@ -40,6 +41,7 @@ def handle_photo(message):
         global position
         global k
         global color
+        global text
         if callback.data == 'top':
             bot.send_message(message.chat.id, f"Хорошо! Теперь отправь мне текст")
             position = 'top'
@@ -76,32 +78,105 @@ def handle_photo(message):
                              reply_markup=markup)
         elif callback.data == 'correct_1':
             k *= 1.5
-            bot.send_message(message.chat.id, f"Хорошо, введи, пожалуйста, текст заново")
-            bot.register_next_step_handler(message, set_photo_text, position, k, color)
+            set_photo_text(message, position, k, color)
         elif callback.data == 'correct_2':
             k /= 1.5
-            bot.send_message(message.chat.id, f"Хорошо, введи, пожалуйста, текст заново")
-            bot.register_next_step_handler(message, set_photo_text, position, k, color)
+            set_photo_text(message, position, k, color)
         elif callback.data == 'correct_3_1':
             color = 1
-            bot.send_message(message.chat.id, f"Хорошо, введи, пожалуйста, текст заново")
-            bot.register_next_step_handler(message, set_photo_text, position, k, color)
+            set_photo_text(message, position, k, color)
         elif callback.data == 'correct_3_2':
             color = 0
-            bot.send_message(message.chat.id, f"Хорошо, введи, пожалуйста, текст заново")
-            bot.register_next_step_handler(message, set_photo_text, position, k, color)
+            set_photo_text(message, position, k, color)
         elif callback.data == 'correct_4':
             position = 'bottom'
-            bot.send_message(message.chat.id, f"Хорошо, введи, пожалуйста, текст заново")
-            bot.register_next_step_handler(message, set_photo_text, position, k, color)
+            set_photo_text(message, position, k, color)
         elif callback.data == 'correct_5':
             position = 'top'
-            bot.send_message(message.chat.id, f"Хорошо, введи, пожалуйста, текст заново")
-            bot.register_next_step_handler(message, set_photo_text, position, k, color)
+            set_photo_text(message, position, k, color)
+
+
+    def set_text(position, k, color, text):
+        try:
+            image = Image.open('photo.jpg')
+            draw = ImageDraw.Draw(image)
+            # загрузка шрифта, поддерживающего кириллицу
+            h = image.height / k
+            font = ImageFont.truetype('CactusClassicalSerif-Regular.ttf',
+                                      h)  # убеждаемся, что файл находится в рабочем каталоге
+
+            # накладываем текст на фото
+
+            ans = []
+            text_2 = text.split()
+            i = 1
+            go = True
+            text = text_2[0]
+            while go:
+                if i < len(text_2):
+                    if (text_2[i][0]).isalpha():
+                        prov = text + text_2[i]
+                        bbox = draw.textbbox((0, 0), prov.upper(), font=font)
+                        text_width = bbox[2] - bbox[0]
+                        if text_width + 60 < image.width:
+                            text = text + " " + text_2[i]
+                            i += 1
+                        else:
+                            text = text.upper()
+                            ans.append(text)
+                            text = text_2[i]
+                            i += 1
+                    else:
+                        text = text + " " + text_2[i]
+                        i += 1
+
+                else:
+                    text = text.upper()
+                    ans.append(text)
+                    go = False
+            if position == 'bottom':
+                n = len(ans)
+            elif position == 'top':
+                n = 0
+
+            for i in ans:
+                bbox = draw.textbbox((0, 0), i, font=font)
+                text_width = bbox[2] - bbox[0]
+                b = (h + 20) * n
+                x = (image.width - text_width) / 2
+                if position == 'bottom':
+                    y = image.height - b
+                    n -= 1
+                elif position == 'top':
+                    y = 20 + b
+                    n += 1
+                if color == 0:
+                    draw.rectangle((x - 8, y, x + text_width + 10, y + h * 1.2), fill=(255, 255, 255, 255))
+                    draw.text((x, y), i, font=font, fill=(0, 0, 0))
+                else:
+                    draw.rectangle((x - 8, y, x + text_width + 10, y + h * 1.2), fill=(0, 0, 0, 0))
+                    draw.text((x, y), i, font=font, fill=(255, 255, 255))
+                image.save('result.jpg')
+
+            bot.send_photo(message.chat.id, open('result.jpg', 'rb'))
+            bot.send_message(message.chat.id, f"Готово!")
+            markup = types.InlineKeyboardMarkup(row_width=2)
+            btn3 = types.InlineKeyboardButton('Да, оставляем так', callback_data='yes')
+            btn4 = types.InlineKeyboardButton('Нет, хочу переделать', callback_data='no')
+            markup.row(btn3, btn4)
+            bot.send_message(message.chat.id, f"{message.from_user.first_name}, тебе нравится?",
+                             reply_markup=markup)
+
+        except Exception as e:
+            bot.send_message(message.chat.id, 'Произошла ошибка, попробуй снова')
+            print(e)
 
 # обработчик текста
+
 def set_photo_text(message, position, k, color):
     try:
+        global text
+        global text_3
         image = Image.open('photo.jpg')
         draw = ImageDraw.Draw(image)
             # загрузка шрифта, поддерживающего кириллицу
@@ -110,8 +185,11 @@ def set_photo_text(message, position, k, color):
                                           h)  # убеждаемся, что файл находится в рабочем каталоге
 
             # накладываем текст на фото
-        text = message.text
-        text_3 = text
+        if text == 0:
+            text = message.text
+            text_3 = text
+        else:
+            text = text_3
         ans = []
         text_2 = text.split()
         i = 1
@@ -172,6 +250,8 @@ def set_photo_text(message, position, k, color):
         markup.row(btn3, btn4)
         bot.send_message(message.chat.id, f"{message.from_user.first_name}, тебе нравится?",
                                  reply_markup=markup)
+
+        return text
 
     except Exception as e:
         bot.send_message(message.chat.id, 'Произошла ошибка, попробуй снова')
